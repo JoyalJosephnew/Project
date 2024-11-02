e607679789bed42d89ca9329a36b017e8ee9960d
JoyalJosephnew <joyaljoseph99p@gmail.com>
2024-11-02 14:07:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-resource "aws_volume_attachment" "attach-1" {
-    device_name = var.device_name
-    volume_id   = var.volume
-    instance_id = var.instance
+resource "aws_ebs_snapshot" "snapshot-1" {
+  volume_id = var.volume
 }
