1528a05a01af414658643ed58876ef7e942fc938
JoyalJosephnew <joyaljoseph99p@gmail.com>
2024-11-03 10:33:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,14 +12,3 @@ variable "name_tag" {
    type        = string
    default     = "ebs-1"
 }
-
-variable "aws_access_key" {
-  type        = string
-  default     = ""
-}
-
-variable "aws_secret_key" {
-  type        = string
-  default     = ""
-}
-
