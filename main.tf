105b87db53cc7ee440ba651da0cda97fc6a24ba7
JoyalJosephnew <joyaljoseph99p@gmail.com>
2024-11-03 12:22:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
-resource "aws_ebs_volume" "ebs-2" {
+resource "aws_ebs_volume" "ebs-1" {
   availability_zone = var.zone
   size              = "10"
+  type              = var.type
+  iops              = 3000
   snapshot_id       = var.snapshot
+  
 
   tags = {
     Name = var.name_tag
