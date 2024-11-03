074a2f4de6a230bb8275d6bae87eb3d7b9ca1a5f
JoyalJosephnew <joyaljoseph99p@gmail.com>
2024-11-03 11:12:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance-1" {
  ami           = var.ami
  instance_type = var.instance_type
  count         = "1"
+ key_name      = var.key
 
  tags = {
    Name = var.name_tag
